64a130689c8630316220f3eb6c3ab89cf87a7c28
Sunny Amrat <sa2812@imperial.ac.uk>
2015-06-14 18:44:27 +0100

    Minor alteration when creating the json file

--- artists.py
+++ artists.py
@@ -20,9 +20,12 @@ print artist_name
 results = d.search(artist_name)
 
 artist = results[0]
-print artist.name, artist.id
+print "Discogs Name: "+artist.name
+print "Artist ID: "+str(artist.id)
 count = artist.releases.count
 pages = int(math.ceil(count/ITEMS_PER_PAGE))
+print count
+print pages
 
 all_releases = []
 for ii in range(1, pages+1):
@@ -33,14 +36,14 @@ for ii in all_releases:
     try:
         try:
             if (artist in ii.artists):
-                artist_releases[ii.id] = [ii.title,
-                                          [jj.name for jj in ii.artists],
-                                          [{kk.title: [ll.id for ll in kk.artists]} for kk in ii.tracklist]]
+                artist_releases[ii.id] = {"title": ii.title,
+                                          "artists": [jj.id for jj in ii.artists],
+                                          "tracks": [{kk.title: [ll.id for ll in kk.artists]} for kk in ii.tracklist]}
         except AttributeError:
             if (artist in ii.main_release.artists):
-                artist_releases[ii.id] = [ii.title,
-                                          [jj.name for jj in ii.main_release.artists],
-                                          [{kk.title: [ll.id for ll in kk.artists]} for kk in ii.tracklist]]
+                artist_releases[ii.id] = {"title": ii.title,
+                                          "artists": [jj.id for jj in ii.main_release.artists],
+                                          "tracks": [{kk.title: [ll.id for ll in kk.artists]} for kk in ii.tracklist]}
     except requests.exceptions.SSLError:
         pass
 
