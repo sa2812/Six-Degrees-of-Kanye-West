203c0b6e6c21ff28558455d5a7304a1a1711b38e
Sunny Amrat <sa2812@imperial.ac.uk>
2015-06-14 16:52:22 +0100

    updates to the functionality of pulling release info. It still seems to be broken and so more tweaks will be needed

--- discogs.py
+++ discogs.py
@@ -9,43 +9,41 @@ import requests
 import time
 import fileinput
 
+ITEMS_PER_PAGE = 50.
+
 d = discogs_client.Client('Networker/0.1', user_token=config.token)
 
 me = d.identity()
 
-artist_name = "Danny Brown"
+artist_name = "Childish Gambino"
 print artist_name
-
 results = d.search(artist_name)
 
 artist = results[0]
+print artist.name
 count = artist.releases.count
-pages = int(math.ceil(count/50.))
+pages = int(math.ceil(count/ITEMS_PER_PAGE))
 print 'Total pages: '+str(pages)
+
+all_releases = []
+for ii in range(1, pages+1):
+    all_releases += artist.releases.page(ii)
 
 artist_releases = {}
 
-ii = 0
-while ii <= pages:
+for jj in all_releases:
     try:
-        a = artist.releases.page(ii)
-        print "Current Page: "+str(ii)
-        for jj in a:
-            if hasattr(jj, "main_release"):
-                try:
-                    rel = jj.main_release
-                    artist_releases[rel.id] = [rel.title, {"artists":[artist.id for artist in rel.artists]}, {"tracks": [track.title for track in rel.tracklist]}]
-                except (requests.exceptions.SSLError, UnicodeEncodeError):
-                    pass
-            elif artist in jj.artists:
-                try:
-                    artist_releases[jj.id] = [jj.title, {"artists":[artist.id for artist in jj.artists]}, {"tracks": [track.title for track in jj.tracklist]}]
-                except (requests.exceptions.SSLError, UnicodeEncodeError):
-                    pass
-    except requests.exceptions.SSLError:
+        try:
+            if artist in jj.artists:
+                print "Release: "+jj.title
+                artist_releases[jj.id] = [jj.title, {"artists":[artist.id for artist in jj.artists]}, {"tracks": [track.title for track in jj.tracklist]}]
+        except AttributeError:
+            rel = jj.main_release
+            if artist in rel.artists:
+                print "Master:  "+rel.title
+                artist_releases[rel.id] = [rel.title, {"artists":[artist.id for artist in rel.artists]}, {"tracks": [track.title for track in rel.tracklist]}]
+    except (requests.exceptions.SSLError, UnicodeEncodeError):
         pass
-    time.sleep(5)
-    ii += 1
 
 with open(artist_name+'.txt', 'w') as outfile:
     json.dump(artist_releases, outfile)
--- test.py
+++ test.py
@@ -15,12 +15,13 @@ d = discogs_client.Client('Networker/0.1', user_token=config.token)
 
 me = d.identity()
 
-artist_name = "Danny Brown"
+artist_name = "Childish Gambino"
 print artist_name
 
 results = d.search(artist_name)
 
 artist = results[0]
+print artist.name
 count = artist.releases.count
 pages = int(math.ceil(count/50.))
 print 'Total pages: '+str(pages)
