0d4a99459ee12c2340418a23071a72acb6a099bf
Sunny Amrat <sa2812@imperial.ac.uk>
2015-06-28 18:07:50 +0100

    all featured artists can now be captured. Now need to think about database storage for all this info

--- spotify.py
+++ spotify.py
@@ -3,16 +3,12 @@ import config
 import spotipy
 import spotipy.util as util
 
-scope = 'user-library-read'
-
 SPOTIPY_CLIENT_ID = config.spotify_id
 SPOTIPY_CLIENT_SECRET = config.spotify_secret
 SPOTIPY_REDIRECT_URI='https://github.com/sa2812/Six-Degrees-of-Kanye-West'
 
-username = "amrat.sunny"
-
-token = util.prompt_for_user_token(username,
-	                               scope,
+token = util.prompt_for_user_token(config.username,
+	                               config.scope,
 	                               client_id = SPOTIPY_CLIENT_ID,
 	                               client_secret = SPOTIPY_CLIENT_SECRET,
 	                               redirect_uri = SPOTIPY_REDIRECT_URI)
@@ -24,8 +20,9 @@ class TrackCollector:
 		self.name = name
 		self.artist_uri = sp.search(q='artist:' + name,
 			                        type='artist')['artists']['items'][0]['uri']
-		self.albums    = self.get_all_albums()
-		self.all_songs = self.get_all_album_tracks()
+		self.albums     = self.get_all_albums()
+		self.all_songs  = self.get_all_album_tracks()
+		self.ft_artists = self.get_all_featured_artists()
 
 	def get_all_albums(self):
 		results = sp.artist_albums(self.artist_uri,
@@ -40,7 +37,8 @@ class TrackCollector:
 	def get_all_album_tracks(self):
 		all_songs = []
 		for ii in self.albums:
-			album_songs = sp.album_tracks(ii['uri'])['items']
+			results = sp.album_tracks(ii['uri'])
+			album_songs = results['items']
 			while results['next']:
 				album_songs.extend(results['items'])
 			ii['tracklist'] = album_songs
@@ -49,6 +47,11 @@ class TrackCollector:
 		return all_songs
 
 	def get_all_featured_artists(self):
+		featured_artists = []
+		for ii in self.all_songs:
+			if len(ii['artists']) > 1:
+				featured_artists.append(ii)
 
+		return featured_artists
 
 a = TrackCollector("Kanye West")
