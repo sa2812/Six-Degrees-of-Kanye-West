1da1d212b3001b625be6c2f400527c7963db4491
Sunny Amrat <sa2812@imperial.ac.uk>
2015-07-08 20:39:23 +0100

    added a tool to generate a big list of artists. next step is to try and add something so that it builds up monitoring generation

--- artist_list_gen.py
+++ artist_list_gen.py
@@ -5,9 +5,23 @@ name   = "Kanye West"
 artist = sp.search(q='artist:' + name,
 			       type='artist')['artists']['items'][0]
 
-artists = [{'name': artist['name'], 'id': artist['id'], 'uri': artist['uri']}]
+artists = [{'name': artist['name'], 'id': artist['id'], 'uri': artist['uri'], 'done': False, 'generation': 0}]
+artist_names = [artist['name']]
 
-rel = sp.artist_related_artists(artist['id'])
-for ii in rel['artists']:
-	artists.append({'name': ii['name'], 'id': ii['id'], 'uri': ii['uri'], 'done': False})
-rel['done'] = True
+not_done = filter(lambda artist: artist['done'] == False, artists)
+
+for jj in artists:
+	if jj['done']:
+		continue
+	related = sp.artist_related_artists(jj['id'])
+	new_gen = jj['generation'] + 1
+	for ii in related['artists']:
+		add_to  = True
+		if ii['name'] in artist_names:
+			add_to = False
+		if add_to:
+			artists.append({'name': ii['name'], 'id': ii['id'], 'uri': ii['uri'], 'done': False, 'generation': new_gen})
+			artist_names.append(ii['name'])
+	jj['done'] = True
+	if len(artists) > 100:
+		break
