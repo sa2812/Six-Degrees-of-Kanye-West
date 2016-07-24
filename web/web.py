ca3a2d49f2a2543463914e9865475fec317b0740
Sunny Amrat <sa2812@imperial.ac.uk>
2016-07-24 13:13:16 +0100

    playing around with Flask urls

--- web/web.py
+++ web/web.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route("/<artist_uri>")
+def degree(artist_uri):
+	return render_template('index.html', uri=artist_uri)
 
 if __name__ == "__main__":
 	app.run()
