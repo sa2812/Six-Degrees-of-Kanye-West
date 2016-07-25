830b938eab1c9bacc745858f3bd179505c2c55dc
Sunny Amrat <sa2812@imperial.ac.uk>
2016-07-25 17:43:10 +0100

    adding track info to the database

--- test.py
+++ test.py
@@ -1,11 +1,27 @@
 from db_conn import *
+import spotipy
+
+sp = spotipy.Spotify()
 
 @db_wrapper
-def search(c, artist):
-	c.execute("""SELECT uri
+def get_track(c):
+	c.execute("""SELECT kanye_degree.'track'
 				 FROM kanye_degree
-				 WHERE name LIKE ?
-		      	 """, ('%'+artist+'%',))
+				 WHERE kanye_degree.'track_done'=0
+				 ORDER BY kanye_degree.'gen' ASC""")
 	return c.fetchone()
 
-print search('jay z')
+@db_wrapper
+def set_track_name(c, track_name, track):
+	c.execute("""UPDATE kanye_degree
+				 SET track_name=?, track_done=1
+				 WHERE track=?""", (track_name, track))
+
+
+current = 0
+while current < 10:
+	track, = get_track()
+	track_name = sp.track(track)['name']
+	print track_name
+	set_track_name(track_name, track)
+	current += 1
